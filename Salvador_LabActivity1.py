5cf4b70a6c30fcfd985c8db1b1ed04344319b1dd
gcfsalvador <145944423+gcfsalvador@users.noreply.github.com>
2023-09-29 07:58:22 +0800

    Add files via upload

---
+++ Salvador_LabActivity1.py
@@ -0,0 +1,75 @@
+lbs = 350
+kg =  lbs/2.205
+kg = round(kg,2)
+lbs = str(lbs)
+kg = str(kg)
+print("Weight in Pounds (lbs) = " + lbs)
+print("Weight converted to Kilogram (kg) = " + kg)
+
+print("==================================")
+
+mi = 21
+km = mi*1.609
+km = round(km,2)
+mi = str(mi)
+km = str(km)
+print("Length in Miles (mi) " + mi)
+print("Length in Kilometer (km) " + km)
+
+print("==================================")
+
+Fahrenheit = 10000
+Celsius = (5/9)*(Fahrenheit - 32)
+Fahrenheit = str(Fahrenheit)
+Celsius = round(Celsius)
+Celsius = str(Celsius)
+print("Temperature to Fahrenheit (°F)" + Fahrenheit)
+print("Temperature to Celsius (°C)" + Celsius)
+
+print("==================================")
+
+Student1 = "25"
+Student2 = "27"
+Student3 = "24"
+Student4 = "25"
+Student5 = "23"
+Student6 = "28"
+Student7 = "26"
+Student8 = "28"
+Student9 = "24"
+Student10 = "23"
+average = "23.23"
+
+print("Age of Student 1: " + Student1)
+print("Age of Student 2: " + Student2)
+print("Age of Student 3: " + Student3)
+print("Age of Student 4: " + Student4)
+print("Age of Student 5: " + Student5)
+print("Age of Student 6: " + Student6)
+print("Age of Student 7: " + Student7)
+print("Age of Student 8: " + Student8)
+print("Age of Student 9: " + Student9)
+print("Age of Student 10: " + Student10)
+print("The average age of the students is: " + average)
+
+print("==================================")
+
+#binabasa mo ba ito maam
+#⊙︿⊙
+
+tropa1 = "<Finn the Fire Swordsman>"
+power1 = "<Weaver of Souls World of Warcraft Weapon Ex-Calibur Calibruner of Camelot The blade the cut down the kingdom of Hell and through it's wake and creation>"
+tropa2 = "<Fiona of the Iceshifters>"
+power2 = "<Controller of Moisture>"
+tropa3 = "<Carbon Dioxide of the Firewatch>"
+power3 = "<Black Coffee Makers>"
+tropa4 = "<Nitrogen Oxide the Barbarian>"
+power4 = "<All for One Magnetism>"
+tropa5 = "<Jupiter Neptune P. Sisyphus Aristotle the 3rd>"
+power5 = "<who controls mineral water and fried noodles>"
+
+print("The story starts with " + tropa1 + " who carries " + power1 + " travelled across the 50 Seas to find friends worthy to be of his companion in the quest to defeat the「Dragon Demon King Man Hero turned Villain King Man」As he travelled across WW2 he found " + tropa2 + " ,She is left as the only survivor in Germany. She joined uhh " + tropa1 + " in his journey as he offered her food in exchange for her tranquil magic. As they travelled again, they meet " + tropa3 + " who is simply camping around watching deers in his tent who can conjure " + power3 + " who is simply camping around watching deers in his tent who can conjure black coffee using his own liquids. After a deer stampede the duo saved him and he offereed his life to serve  " + tropa1 + "Then they meet " + tropa4 + "ho is the ex-wife of Carbon Dioxide, Her power is " + power4 + "in contrast to Carbon Dioxide who can control black coffee. After a not so through battle she agreed join in under 1 condition which is to force a wedding among Carbon Dioxide and Her. A simple shotgun wedding in which " + tropa1 + "simply accepts without arguement. " + tropa3 + "feared for everything when he learned about it. As they walk across the moon they met " + tropa5 + " a wondering magician that can conduct " + power5 + " He can only control the boiling point and temperature of an object using mineral water and fried noodles. He joined because he was bored of living Andromeda." + tropa1 + "who is already choking due to the lack of oxygen of the Moon just accepted it.") 
+print("""In a climatic fashion, Fiona used her Ice powers to control the moisture of objects towards the 「Dragon Demon King Man Hero turned Villain King Man」's phone which ruined his mood and turned feral. 「Dragon Demon King Man Hero turned Villain King Man」slapped Carbon Dioxide towards a Phosphorus Banner and he exploded Yamcha style. Jupiter Naptune P. Sisyphus Aristotle the III used his power of mineral water and fried noodles to ruin his only source of food.
+      Nitrogen Oxide the Barbarian uses All for One which made her a Magnet for the「Dragon Demon King Man Hero turned Villain King Man」weapon in which she got flung around until she hit a wall. Finn the Fire Swordsman man used his Excaliber Caliburner of Camelot to strike the heart of
+      「Dragon Demon King Man Hero turned Villain King Man」's heart and he just died right there dead and stuff like dead gravestone dead dead. And they celebrated in Finn the Fire Swordsman's Home in which his mom cooked food. The story ends with 2 kids, a divorced family, and a homeless man.
+      The End.""")
